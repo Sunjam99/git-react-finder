a1a8cadced9eb5ef5288e2ccd57ccf9c397559a0
Sunjam99 <simzmusaz@gmail.com>
2020-07-08 15:30:22 +0200

    Update version

--- src/components/Pages/about.js
+++ src/components/Pages/about.js
@@ -5,7 +5,7 @@ export const about = () => {
         <Fragment>
             <h1>About this app</h1>
             <p>App to search Github users</p>
-            <p>version 1.0.0</p>
+            <p>version 1.1.0</p>
         </Fragment>
     )
 }
